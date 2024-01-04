e1bb808c4d0d2da254a3568bcbeb4c99bbf62701
Spartak2 <orkotop030@gmail.com>
2024-01-05 01:59:38 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+"use strict";
+function profile(username, playTime) {
+
+    const profile = {
+        username: "Jacob",
+        playTime: 300,
+        changeUsername: function(newName) {
+            this.username += newName;
+        },
+        updatePlayTime: function(hours) {
+            this.playTime += hours;
+        },
+        getInfo: function(){
+            return `${this.username} has ${this.playTime} active hours!`;
+        }
+    };
+    return profile;
+}
+
+const userProfile = profile("Jacob", 300);
+
+console.log(userProfile.getInfo()); // "Jacob has 300 active hours!"
+
+userProfile.changeUsername("Marco");
+console.log(userProfile.getInfo()); // "Marco has 300 active hours!"
+
+userProfile.updatePlayTime(20);
+console.log(userProfile.getInfo()); // "Marco has 320 active hours!"
